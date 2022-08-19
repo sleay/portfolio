e691d80299dcf99cff41bb525ab3232c12eb3be8
sleay <105851917+sleay@users.noreply.github.com>
2022-08-19 14:16:22 +0900

    test

---
+++ js/pr.js
@@ -0,0 +1,71 @@
+let data='';
+$.ajax({
+    
+    url:'../js/data.json',
+
+    success:function(json){
+        data=json;
+
+if(localStorage.getItem('k2')){
+    let key = localStorage.getItem('k2')
+
+    $('video').attr('src',`${data.project[key].video}`)
+    setTimeout(function(){
+        localStorage.removeItem('k2')
+    },50)
+}
+       
+
+
+       
+        
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+history.scrollRestoration = "manual";
+if($(window).scrollTop() == 0){
+
+
+    $(window).bind('wheel', function(event){
+        (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) ? state = true :  state = false;
+    aa()
+    });
+}  
+
+function aa(){
+    if(!state){
+        $('.frame_box').addClass('active')
+        $('.contain1 > h2').fadeOut();
+        setTimeout(function(){
+            $('body').css({overflow:'auto'})
+            $('header').fadeIn();
+        },1000)
+        $('video').get(0).play();
+    }
+}
+
+$(window).on('scroll',function(){
+    if($(window).scrollTop() > 0){
+        $('video').get(0).pause();
+    }
+    else{
+        $('video').get(0).play();
+    }
+})
+
+
